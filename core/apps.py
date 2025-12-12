6c3a3414112604f3a406c31283f305b837b6bf76
Ilia <phanoix@gmail.com>
2025-12-12 15:28:25 -0500

    rabbitmq won't be needed and this is causing issues with the django upgrade

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-import pika
-from constance import config
 
 
 class CoreConfig(AppConfig):
@@ -9,12 +7,3 @@ class CoreConfig(AppConfig):
     def ready(self):
         # Makes sure all signal handlers are connected
         from core import handler
-
-
-        if config.SERVICE_MESH_ACTIVATION:
-            credentials = pika.PlainCredentials(config.SERVICE_MESH_USER, config.SERVICE_MESH_PASSWORD)
-            connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.SERVICE_MESH_HOST, credentials=credentials))
-            channel = connection.channel()
-            channel.exchange_declare(exchange='account', exchange_type='topic', durable=True)
-
-            connection.close()
